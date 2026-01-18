ed76f909960b1dc31a1e74fd6bbb371dc5c1411e
ivanVanko910 <ivanvanko910@gmail.com>
2026-01-18 12:12:07 +0530

    test get selected airport details

--- src/main/java/lk/ijse/cmjd113/AirTicketCollector/AirTicketCollectorApplication.java
+++ src/main/java/lk/ijse/cmjd113/AirTicketCollector/AirTicketCollectorApplication.java
@@ -10,4 +10,4 @@ public class AirTicketCollectorApplication {
 		SpringApplication.run(AirTicketCollectorApplication.class, args);
 	}
 
-}
+} 
--- src/main/java/lk/ijse/cmjd113/AirTicketCollector/controller/AirportController.java
+++ src/main/java/lk/ijse/cmjd113/AirTicketCollector/controller/AirportController.java
@@ -1,13 +1,11 @@
 package lk.ijse.cmjd113.AirTicketCollector.controller;
 
 import lk.ijse.cmjd113.AirTicketCollector.dto.AirportDTO;
+import lk.ijse.cmjd113.AirTicketCollector.util.IDGenerate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/airports")
@@ -16,6 +14,19 @@ public class AirportController {
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AirportDTO> saveAirport(
             @RequestBody AirportDTO airportDTO){
+        airportDTO.setAirportId(IDGenerate.airportId());
         return new ResponseEntity<>(airportDTO, HttpStatus.CREATED);
+    }
+    //Get Selected Airport
+    @GetMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<AirportDTO> getSelectedAirport(@PathVariable ("id") String airportId){
+        var airportDTO = new AirportDTO(
+                airportId,
+                "CMB",
+                "Bandaranayake International Airport",
+                "Katunayaka",
+                "SL"
+        );
+        return new ResponseEntity<>(airportDTO,HttpStatus.OK);
     }
 }
