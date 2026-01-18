912149ce506b999aa5f79356aa8a5491b9b98ddd
ivanVanko910 <ivanvanko910@gmail.com>
2026-01-18 10:53:09 +0530

    create and test save airports

--- src/main/java/lk/ijse/cmjd113/AirTicketCollector/controller/AirportController.java
+++ src/main/java/lk/ijse/cmjd113/AirTicketCollector/controller/AirportController.java
@@ -1,10 +1,21 @@
 package lk.ijse.cmjd113.AirTicketCollector.controller;
 
+import lk.ijse.cmjd113.AirTicketCollector.dto.AirportDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/airports")
 public class AirportController {
-
+    // Save an airport
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<AirportDTO> saveAirport(
+            @RequestBody AirportDTO airportDTO){
+        return new ResponseEntity<>(airportDTO, HttpStatus.CREATED);
+    }
 }
